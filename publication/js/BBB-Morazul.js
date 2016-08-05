89f69de4c28b3d8f04f3a561f5381908af8e726d
brm-castillol <esteffany.castillo@brm.com.co>
2016-08-05 18:19:37 -0500

    animacion

--- publication/js/BBB-Morazul.js
+++ publication/js/BBB-Morazul.js
@@ -1,3 +1,14 @@
 jQuery(document).ready(function($) {
 	jQuery("#form-datosAP").validate();	
 });
+
+  // Elements
+  var scene = document.getElementById('scene');
+  var clipCheckbox = document.getElementById('clip');
+  var relativeCheckbox = document.getElementById('relative');
+
+  // Pretty simple huh?
+  var parallax = new Parallax(scene, {
+    relativeInput: relativeCheckbox.checked,
+    clipRelativeInput: clipCheckbox.checked
+  });

28ca28cac0f324edea0d1d7c0693d6469eef27df
brm-castillol <esteffany.castillo@brm.com.co>
2016-08-05 19:36:16 -0500

    actuali

--- publication/js/BBB-Morazul.js
+++ publication/js/BBB-Morazul.js
@@ -12,3 +12,14 @@ jQuery(document).ready(function($) {
     relativeInput: relativeCheckbox.checked,
     clipRelativeInput: clipCheckbox.checked
   });
+
+    // Elements
+  var scene2 = document.getElementById('scene2');
+  var clipCheckbox2 = document.getElementById('clip2');
+  var relativeCheckbox2 = document.getElementById('relative2');
+
+  // Pretty simple huh?
+  var parallax2 = new Parallax(scene2, {
+    relativeInput: relativeCheckbox2.checked,
+    clipRelativeInput: clipCheckbox2.checked
+  });
